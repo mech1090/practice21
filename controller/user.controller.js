34db97ee7cc1f108df59a014499e310e2f430740
Shahzad <mech1090@gmail.com>
2022-07-01 18:23:20 -0600

    signup logic done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,17 +1,37 @@
-
+const {userValidationSchema} = require('../validation/user.validation')
+const userService = require('../service/user.service')
+const bcrypt = require('bcrypt')
+const config = require('config')
+const { has } = require('config')
 
 
 const getLoginForm = (req,res)=>{
     return res.render('login/layout')
 }
 
-const login = (req,res)=>{}
+const login = async(req,res)=>{
+    
+
+}
 
 const getSignupForm = (req,res)=>{
     return res.render('signup/layout')
 }
 
-const signup =(req,res)=>{
+const signup =async(req,res)=>{
+    const {email,password} = req.body
+    const fields = {email,password}
+    const {error,value} = userValidationSchema(fields)
+    if(error){
+        return res.render('signup/layout',{message:error.details[0].message})
+    }
+    const findUser = await userService.getEmail({email})
+    if(findUser){
+        return res.render('login/layout',{message:'User exist Login Here'})
+    }
+    const hashPassword = await bcrypt.hash(password,config.get('hash.salt'))
+    const createUser = await userService.createFields({email,password:hashPassword})
+    return res.render('signup/layout',{message:'User Created'})
 
 }
 
